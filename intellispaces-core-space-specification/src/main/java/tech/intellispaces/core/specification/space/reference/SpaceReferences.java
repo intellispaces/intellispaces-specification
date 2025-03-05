1e0217b8046bfca2cea267298716892b0a14e1bb
lavor <lavor.andrey@gmail.com>
2025-03-05 11:06:37 +0600

    Update

--- intellispaces-core-space-specification/src/main/java/tech/intellispaces/core/specification/space/reference/SpaceReferences.java
+++ intellispaces-core-space-specification/src/main/java/tech/intellispaces/core/specification/space/reference/SpaceReferences.java
@@ -2,6 +2,10 @@ package tech.intellispaces.core.specification.space.reference;
 
 public interface SpaceReferences {
 
+  static SpaceReference withName(String name) {
+    return new SpaceReferenceImpl(name);
+  }
+
   static SpaceReferenceBuilder build() {
     return new SpaceReferenceBuilder();
   }
